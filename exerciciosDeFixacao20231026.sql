460f3a9d1a3d37cf751b465d5e0db14c33f67c79
TPL777 <143000724+TPL777@users.noreply.github.com>
2023-10-26 20:58:25 -0300

    Trigger para tentativa de exclusão de um cliente na tabela Clientes:

--- exerciciosDeFixacao20231026.sql
+++ exerciciosDeFixacao20231026.sql
@@ -8,3 +8,15 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+DELIMITER //
+CREATE TRIGger tentativa_exclusao_cliente
+BEFORE DELETE ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem)
+    VALUES (CONCAT('Tentativa de exclusão de cliente em ', NOW()));
+   
+END;
+//
+DELIMITER ;
